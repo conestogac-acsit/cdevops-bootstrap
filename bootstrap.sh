804a94324defb5baeef918a5853361e0998abbf2
Rich Hildred <rhildred@conestogac.on.ca>
2025-03-13 17:42:21 +0000

    fininsh installing docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,8 +13,9 @@ if [ $(id -u) -eq 0 ]; then
 	echo \
 	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
 	    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-	      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	sudo apt-get update
+	      tee /etc/apt/sources.list.d/docker.list > /dev/null
+	apt-get update && apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	systemctl start docker
 else
 	echo "This needs to be run as root. You can get root by running sudo su -"
 fi
